b35279bcad909ecc6d88ace727d442b79481cf5f
nvhungw <119866957+nvhungw@users.noreply.github.com>
2023-04-29 23:11:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# B-i-9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-x1aj8g)
---
+++ script.js
@@ -0,0 +1,33 @@
+//Lấy 2 ô input
+let firstNumber = document.getElementById('firstNumber');
+let secondNumber = document.getElementById('secondNumber');
+let resultElement = document.getElementById('result');
+let congBtn = document.getElementById('congBtn');
+
+//Viết hàm Cộng+
+// function add() {
+//   let first = +firstNumber.value; //Thêm dấu + để đổi sang dạng số
+//   let second = +secondNumber.value; //Thêm dấu + để đổi sang dạng số
+//   let result = first + second;
+//   resultElement.innerText = `${first} + ${second} = ${result}`;
+// }
+
+//Hoặc có thể viết hàm Cộng+ như này (bỏ onclick ở button Cộng+)
+congBtn.addEventListener(
+  'click',
+  function () //Bao gồm: click, change, keypress, keyup, keydown
+  {
+    let first = +firstNumber.value; //Thêm dấu + để đổi sang dạng số
+    let second = Number(secondNumber.value); //Thêm Number() để đổi sang dạng số
+    let result = first + second;
+    resultElement.innerText = `${first} + ${second} = ${result}`;
+  }
+);
+
+//Viết hàm Trừ-
+function tru() {
+  let first = +firstNumber.value; //Thêm dấu + để đổi sang dạng số
+  let second = +secondNumber.value; //Thêm dấu + để đổi sang dạng số
+  let result = first - second;
+  resultElement.innerText = `${first} - ${second} = ${result}`;
+}
